fc57318487bba598579e32e5870012a594f2da10
Luis Chodiman <lechodiman@uc.cl>
2020-12-12 22:48:32 -0300

    feat: add query filter to list products controller

--- src/api/products/controllers/listProducts.ts
+++ src/api/products/controllers/listProducts.ts
@@ -1,8 +1,39 @@
 import Product from '../models/Product';
+import * as z from 'zod';
 import { Request, Response } from 'express-serve-static-core';
 
+const ListQuerySchema = z.object({
+  search: z.string().min(3).optional(),
+});
+
 const listProducts = async (req: Request, res: Response) => {
-  const products = await Product.find();
+  const { search = '.*' } = req.query as z.infer<typeof ListQuerySchema>;
+
+  const products = await Product.find({
+    $or: [
+      {
+        brand: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+      {
+        productId: search,
+      },
+      {
+        description: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+      {
+        name: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+    ],
+  });
 
   res.json({
     products,
