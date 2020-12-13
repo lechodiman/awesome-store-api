0c70227c51b6e08729daee0813780df9da6b21c8
Luis Chodiman <lechodiman@uc.cl>
2020-12-12 22:54:33 -0300

    chore: add types to update product controller

--- src/api/products/controllers/updateProduct.ts
+++ src/api/products/controllers/updateProduct.ts
@@ -1,17 +1,27 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { deleteFile } from '../../../config/multer';
-import Product from '../models/Product';
+import Product, { ProductSchema } from '../models/Product';
 import { Request, Response } from 'express-serve-static-core';
+import * as z from 'zod';
+
+const updateProductSchema = ProductSchema.omit({
+  productId: true,
+  imageUrl: true,
+}).partial();
 
 const updateProduct = async (req: Request, res: Response) => {
   const product = await Product.findOne({ productId: req.params.productId });
 
-  const params = req.body;
+  const params = req.body as z.infer<typeof updateProductSchema>;
 
   let imageUrl = product.imageUrl;
+
   if (req.file) {
     await cloudinary.uploader.destroy(product.imageUrl);
     const uploadedImage = await cloudinary.uploader.upload(req.file.path);
+
+    deleteFile(req.file.path);
+
     imageUrl = uploadedImage.url;
   }
 
@@ -24,12 +34,8 @@ const updateProduct = async (req: Request, res: Response) => {
     { omitUndefined: true, new: true }
   );
 
-  if (req.file) {
-    deleteFile(req.file.path);
-  }
-
   res.json({
-    message: 'Update',
+    product: updatedProduct,
   });
 };
 
